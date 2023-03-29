cdf7e9ebca9494167dd0b315aa88df8b512898a9
Nathalia Limo <se191010@fhstp.ac.at>
2023-03-29 11:26:56 +0200

    add mobile navbar

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -12,7 +12,7 @@ const navigation = [
 
 export default function Nav() {
   const [color, setColor] = useState('transparent');
-  const [textColor, setTextColor] = useState('black');
+  const [textColor, setTextColor] = useState('#f5f7f7');
   const [borderColor, setBorderColor] = useState('transparent');
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
 
@@ -20,7 +20,7 @@ export default function Nav() {
     const changeColor = () => {
       if (window.scrollY >= 90) {
         setColor('#000407');
-        setTextColor('#f8f8f8');
+        setTextColor('#f5f7f7');
         setBorderColor('#edb518');
       } else {
         setColor('transparent');
@@ -49,9 +49,8 @@ export default function Nav() {
           <div className="flex lg:hidden">
             <button
               type="button"
-              className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-400"
+              className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-white"
               onClick={() => setMobileMenuOpen(true)}
-              style={{ color: `${textColor}` }}
             >
               <span className="sr-only">Open main menu</span>
               <Bars3Icon className="h-6 w-6" aria-hidden="true" />
@@ -63,7 +62,7 @@ export default function Nav() {
                 key={item.name}
                 href={item.href}
                 style={{ color: `${textColor}` }}
-                className="text-sm font-semibold leading-6 text-white"
+                className="text-sm font-semibold leading-6"
               >
                 {item.name}
               </a>
@@ -72,30 +71,25 @@ export default function Nav() {
         </nav>
         <Dialog as="div" className="lg:hidden" open={mobileMenuOpen} onClose={setMobileMenuOpen}>
           <div className="fixed inset-0 z-50" />
-          <Dialog.Panel className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-gray-900 px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-white/10">
+          <Dialog.Panel className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-primary px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-white/10">
             <div className="flex items-center justify-between">
               <a href="#" className="-m-1.5 p-1.5">
-                <span className="sr-only">Your Company</span>
-                <button className="rounded-full bg-white p-3 logo">NL</button>
+                <span className="sr-only">NL</span>
+                <button className="rounded-full bg-secondary p-3 logo">NL</button>
               </a>
-              <button
-                type="button"
-                className="-m-2.5 rounded-md p-2.5 text-gray-400"
-                onClick={() => setMobileMenuOpen(false)}
-                style={{ color: `${textColor}` }}
-              >
+              <button type="button" className="-m-2.5 rounded-md p-2.5 text-white" onClick={() => setMobileMenuOpen(false)}>
                 <span className="sr-only">Close menu</span>
                 <XMarkIcon className="h-6 w-6" aria-hidden="true" />
               </button>
             </div>
             <div className="mt-6 flow-root">
-              <div className="-my-6 divide-y divide-gray-500/25">
+              <div className="-my-6 divide-y divide-secondary">
                 <div className="space-y-2 py-6">
                   {navigation.map((item) => (
                     <a
                       key={item.name}
                       href={item.href}
-                      className="-mx-3 block rounded-lg py-2 px-3 text-base font-semibold leading-7 text-white hover:bg-gray-800"
+                      className="-mx-3 block rounded-lg py-2 px-3 text-base font-semibold leading-7 text-white"
                     >
                       {item.name}
                     </a>
